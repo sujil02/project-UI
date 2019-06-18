d1ddcf1fce034cc0dbfa3c074951de190fcdfb3e
rahulbhat31 <rahulbhat1203@yahoo.in>
2019-06-18 02:52:26 -0400

    added private profile things

--- src/components/RegisterComponent.js
+++ src/components/RegisterComponent.js
@@ -1,5 +1,9 @@
 import React from 'react';
-const RegisterComponent = () => {
+import {Link} from 'react-router-dom';
+
+
+
+const RegisterComponent = (user) => {
     return (
         <div>
             <div className="row justify-content-center">
@@ -14,7 +18,12 @@ const RegisterComponent = () => {
                                     <h5> First Name: </h5>
                                 </label>
                                 <div className="input-group">
-                                    <input type="text" className="form-control" placeholder="First Name" />
+                                    <input type="text" className="form-control"
+                                           placeholder="First Name"
+                                           onChange={(event) => user.updateUser(
+                                               user=
+                                                   { ...user.updatedUser,
+                                                       firstName: event.target.value})}/>
                                 </div>
                             </div>
                             <div className="form-group">
@@ -22,7 +31,12 @@ const RegisterComponent = () => {
                                     <h5> Last Name: </h5>
                                 </label>
                                 <div className="input-group">
-                                    <input type="text" className="form-control" placeholder="Last Name" />
+                                    <input type="text" className="form-control"
+                                           placeholder="Last Name"
+                                           onChange={(event) => user.updateUser(
+                                               user=
+                                                   { ...user.updatedUser,
+                                                       lastName: event.target.value})}/>
                                 </div>
                             </div>
                             <div className="form-group">
@@ -33,7 +47,11 @@ const RegisterComponent = () => {
                                     <div className="input-group-prepend">
                                         <div className="input-group-text"><i className="fa fa-user" /></div>
                                     </div>
-                                    <input type="text" className="form-control" placeholder="Username" />
+                                    <input type="text" className="form-control" placeholder="Username"
+                                           onChange={(event) => user.updateUser(
+                                               user=
+                                                   { ...user.updatedUser,
+                                                       username: event.target.value})}/>
                                 </div>
                             </div>
                             <div className="form-group">
@@ -44,52 +62,130 @@ const RegisterComponent = () => {
                                     <div className="input-group-prepend">
                                         <div className="input-group-text"><i className="fa fa-lock" /></div>
                                     </div>
-                                    <input type="password" className="form-control" placeholder="Password" />
+                                    <input type="password" className="form-control"
+                                           placeholder="Password"
+                                           onChange={(event) => user.updateUser(
+                                               user=
+                                                   { ...user.updatedUser,
+                                                       password: event.target.value})}/>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label className="col-form-label-sm">
-                                    <h5> Account Type: </h5>
+                                    <h5> Role : </h5>
                                 </label>
                                 <div className="input-group">
-                                    <select className="form-control">
-                                        <option value="APPLICANT"> Applicant </option>
+                                    <select className="form-control"
+                                            onChange={(event) => user.updateUser(
+                                                user = {
+                                                    ...user.updatedUser,
+                                                    role: event.target.value
+                                                })}
+                                            value={user.updatedUser.role}>
+                                        <option value="STUDENT"> Student </option>
                                         <option value="RECRUITER"> Recruiter </option>
                                     </select>
                                 </div>
                             </div>
 
-                            <div className="form-group">
-                                <label className="col-form-label-sm">
-                                    <h5> Qualification: </h5>
-                                </label>
-                                <div className="input-group">
-                                    <select className="form-control">
-                                        <option value="GRADUATE"> Graduate </option>
-                                        <option value="BACHELOR"> Bachelor </option>
-                                    </select>
-                                </div>
-                            </div>
 
-                            <div className="form-group">
-                                <label className="col-form-label-sm">
-                                    <h5> Major in: </h5>
-                                </label>
+
+
+                            {
+                                user.updatedUser.role === "STUDENT" &&
+
+                                <div>
+                                    <div className="form-group">
+                                        <label className="col-form-label-sm">
+                                            <h5> Major in: </h5>
+                                        </label>
+                                        <div className="input-group">
+                                            <select className="form-control">
+                                                <option value="CS"> Computer Science </option>
+                                                <option value="IS"> Information Systems </option>
+                                                <option value="ECE"> Electrical Engineering </option>
+                                                <option value="DS"> Data Science </option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                <div className="form-group">
+                                    <label className="col-form-label-sm">
+                                        <h5> Qualification: </h5>
+                                    </label>
                                 <div className="input-group">
-                                    <select className="form-control">
-                                        <option value="CS"> Computer Science </option>
-                                        <option value="IS"> Information Systems </option>
-                                        <option value="ECE"> Electrical Engineering </option>
-                                        <option value="DS"> Data Science </option>
-                                    </select>
+                                <select className="form-control"  placeholder="Select Role" >
+                                    <option value="GRADUATE"> Graduate </option>
+                                    <option value="BACHELOR"> Bachelor </option>
+                                </select>
                                 </div>
-                            </div>
+                                </div>
+                                <div className="form-group">
+                                    <label className="col-form-label-sm">
+                                        <h5> University: </h5>
+                                    </label>
+                                <div className="input-group">
+                                <div className="input-group-prepend">
+                                    <div className="input-group-text"><i className="fa fa-university"></i></div>
+                                    </div>
+                                    <input type="password" className="form-control"
+                                        placeholder="University"
+                                        onChange={(event) => user.updateUser(
+                                    user=
+                                        { ...user.updatedUser,
+                                            university: event.target.value})}/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label className="col-form-label-sm">
+                                        <h5> GPA: </h5>
+                                    </label>
+                                    <div className="input-group">
+                                <div className="input-group-prepend">
+                                <div className="input-group-text"><i className="fas fa-poll-h"></i></div>
+                                </div>
+                                        <input type="password" className="form-control"
+                                        placeholder="GPA"
+                                        onChange={(event) => user.updateUser(
+                                        user=
+                                            { ...user.updatedUser,
+                                                gpa: event.target.value})}/>
+                                </div>
+                                </div>
+                                </div>
+
+                            }
+
+                            {
+                                user.updatedUser.role === "RECRUITER" &&
+
+                                <div className="form-group">
+                                    <label className="col-form-label-sm">
+                                        <h5> Company: </h5>
+                                    </label>
+                                    <div className="input-group">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text"><i className="fa fa-user" /></div>
+                                        </div>
+                                        <input type="text" className="form-control" placeholder="Company Name"
+                                               onChange={(event) => user.updateUser(
+                                                   user=
+                                                       { ...user.updatedUser,
+                                                           company: event.target.value})}/>
+                                    </div>
+                                </div>
+                            }
 
                         </form>
                     </div>
                     <div className="card-footer">
                         <div className="form-row">
-                            <button className="btn btn-success ml-1 mr-auto"> Register </button>
+                            <Link className="btn btn-success ml-1 mr-auto"
+                            onClick={() => user.register(user.updatedUser)}
+                            to ="/login">
+
+                                Register
+
+                            </Link>
                         </div>
 
                     </div>
---
+++ src/container/RegisterUserContainer.js
@@ -0,0 +1,31 @@
+import {connect} from 'react-redux'
+import RegisterComponent from "../components/RegisterComponent";
+import UserService from '../services/UserService'
+let userService = UserService.getInstance();
+
+
+const stateTOPropertyMapper = state => ({
+    updatedUser: state.registerUserReducer.user
+});
+
+const propertyToDispatchMapper = dispatch => ({
+
+    updateUser : (user) => {
+        dispatch({
+            type: 'UPDATE_USER',
+            user: user
+        })
+    },
+
+    register: (user) =>
+        userService.registerUser(user)
+            .then(result => dispatch({
+                type: 'SAVED_USER',
+                user: result
+            }))
+
+});
+
+const RegisterUserContainer = connect(stateTOPropertyMapper, propertyToDispatchMapper)(RegisterComponent);
+
+export default  RegisterUserContainer;
