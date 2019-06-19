f83e000ac93b26c664f0e1c20d8df32bc59ced8e
rahulbhat31 <rahulbhat1203@yahoo.in>
2019-06-18 21:42:28 -0400

    added private profile page

--- src/container/PrivateProfileContainer.js
+++ src/container/PrivateProfileContainer.js
@@ -6,12 +6,18 @@ let userService = UserService.getInstance();
 
 const stateTOPropertyMapper = state => ({
     loggedInUser: state.loginReducer.user,
-    updatedUser: state.privateProfileReducer.user
+    updatedUser: state.privateProfileReducer.user,
+    tab: state.privateProfileReducer.tab,
+    savedGitJobs : state.privateProfileReducer.savedGitJobs,
+    allAddedJobs : state.privateProfileReducer.allAddedJobs,
+    followedStudents : state.privateProfileReducer.followedStudents,
+    markedStudents : state.privateProfileReducer.markedStudents
+
 });
 
 const propertyToDispatchMapper = dispatch => ({
 
-    updateUser : (user) => {
+    updateUser: (user) => {
         dispatch({
             type: 'UPDATE_USER',
             user: user
@@ -23,7 +29,183 @@ const propertyToDispatchMapper = dispatch => ({
             .then(result => dispatch({
                 type: 'SAVE_USER',
                 user: result
-            }))
+            })),
+
+    getSavedGitJobs: (user) => {
+
+    dispatch({
+                 type: 'SAVED_GIT_JOBS',
+                 savedGitJobs: [
+                     {
+                         "id": "3",
+                         "type": null,
+                         "url": null,
+                         "createdAt": null,
+                         "company": "Google",
+                         "company_url": null,
+                         "address": "NY",
+                         "title": "ABC",
+                         "description": null,
+                         "applicationDetails": "aa",
+                         "logoDetails": null
+                     },
+                     {
+                         "id": "4",
+                         "type": "IT",
+                         "url": null,
+                         "createdAt": null,
+                         "company": "Facebook",
+                         "company_url": "facebook.com",
+                         "address": "BOSTON",
+                         "title": "Software Developer",
+                         "description": "Software Developer",
+                         "applicationDetails": "Hello",
+                         "logoDetails": null
+                     }
+                 ],
+                 user: user,
+                 tab: 'SAVED_GIT_JOBS'
+             })
+
+
+
+},
+
+
+    getAllAddedJobs: (user) => {
+
+            dispatch({
+                type: 'ALL_ADDED_JOBS',
+                allAddedJobs: [
+                    {
+                        "id": "3",
+                        "type": null,
+                        "url": null,
+                        "createdAt": null,
+                        "company": "Google",
+                        "company_url": null,
+                        "address": "NY",
+                        "title": "ABC",
+                        "description": null,
+                        "applicationDetails": "aa",
+                        "logoDetails": null
+                    },
+                    {
+                        "id": "4",
+                        "type": "IT",
+                        "url": null,
+                        "createdAt": null,
+                        "company": "Facebook",
+                        "company_url": "facebook.com",
+                        "address": "BOSTON",
+                        "title": "Software Developer",
+                        "description": "Software Developer",
+                        "applicationDetails": "Hello",
+                        "logoDetails": null
+                    }
+                ],
+                user: user,
+                tab: 'ALL_ADDED_JOBS'
+            })
+
+
+
+        },
+
+    getMarkedStudents: (user) => {
+
+        dispatch({
+            type: 'MARKED_STUDENTS',
+            markedStudents: [
+                {
+                    "id": "3",
+                    "type": null,
+                    "url": null,
+                    "createdAt": null,
+                    "company": "Google",
+                    "company_url": null,
+                    "address": "NY",
+                    "title": "ABC",
+                    "description": null,
+                    "applicationDetails": "aa",
+                    "logoDetails": null
+                },
+                {
+                    "id": "4",
+                    "type": "IT",
+                    "url": null,
+                    "createdAt": null,
+                    "company": "Facebook",
+                    "company_url": "facebook.com",
+                    "address": "BOSTON",
+                    "title": "Software Developer",
+                    "description": "Software Developer",
+                    "applicationDetails": "Hello",
+                    "logoDetails": null
+                }
+            ],
+            user: user,
+            tab: 'MARKED_STUDENTS'
+        })
+
+
+
+    },
+
+    getFollowedStudents: (user) => {
+
+        dispatch({
+            type: 'FOLLOWED_STUDENT',
+            followedStudents: [
+                {
+                    "id": "3",
+                    "type": null,
+                    "url": null,
+                    "createdAt": null,
+                    "company": "Google",
+                    "company_url": null,
+                    "address": "NY",
+                    "title": "ABC",
+                    "description": null,
+                    "applicationDetails": "aa",
+                    "logoDetails": null
+                },
+                {
+                    "id": "4",
+                    "type": "IT",
+                    "url": null,
+                    "createdAt": null,
+                    "company": "Facebook",
+                    "company_url": "facebook.com",
+                    "address": "BOSTON",
+                    "title": "Software Developer",
+                    "description": "Software Developer",
+                    "applicationDetails": "Hello",
+                    "logoDetails": null
+                }
+            ],
+            user: user,
+            tab: 'FOLLOWED_STUDENT'
+        })
+
+
+
+    },
+
+
+
+
+
+
+    changeTab : (user , tab) => {
+        dispatch({
+            type: 'CHANGED_TAB',
+            user: user,
+            tab: tab
+        })
+
+    },
+
 
 });
 
